7e915a3a60a3b180f488381a8279fa71f351d997
Thomas Chaplin <thomaschaplin@outlook.com>
2022-03-24 16:18:51 +0000

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use lib::generate;
 use lib::get_count;
 
 fn main() {
-    let count = get_count();
+    let count1 = get_count();
     let counter_string = generate(count);
 
     println!("{}", counter_string);
