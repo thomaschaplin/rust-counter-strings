87b1c4aed3b37a36a0e046f426d6a83d5d38667d
Thomas Chaplin <thomaschaplin@outlook.com>
2023-06-08 23:30:54 +0100

    remove lib

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,51 @@
-mod lib;
-use lib::generate;
-use lib::get_count;
+use std::env;
+use unicode_segmentation::UnicodeSegmentation;
 
 fn main() {
     let count = get_count();
     let counter_string = generate(count);
 
     println!("{}", counter_string);
+}
+
+pub fn generate(mut count: usize) -> String {
+    let mut counter_string = String::new();
+
+    while count > 0 {
+        let mut result = String::from("*");
+
+        result.push_str(&count.to_string().graphemes(true).rev().collect::<String>());
+
+        if result.len() > count {
+            result = result[..count].to_string();
+        }
+
+        counter_string.push_str(&result);
+
+        count -= result.len();
+    }
+
+    return counter_string.graphemes(true).rev().collect::<String>();
+}
+
+pub fn get_count() -> usize {
+    let args: Vec<String> = env::args().collect();
+    let count: usize = match args[1].trim().parse() {
+        Ok(num) => num,
+        Err(_) => panic!(
+            "Failed to read value \"{}\", we were expecting a number!",
+            args[1].trim()
+        ),
+    };
+    count
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_generate() {
+        assert_eq!(generate(10), "*3*5*7*10*");
+    }
 }
